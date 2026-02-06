728276458661765407d12554fa77e8912ddf86f4
Asmaul Mallick <asmaulmallick360@gmail.com>
2026-02-06 13:40:41 +0530

    deleted old root files

--- src/utils/asyncHandler.js
+++
@@ -1,6 +0,0 @@
-const asyncHandler = (requestHandler) => {
-  return async (req, res, next) => {
-    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-  };
-};
-export default asyncHandler;
